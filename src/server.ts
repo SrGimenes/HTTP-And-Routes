e7aa765b4ea6ace537282565c9a57acaefcc2dc5
GabriellGimenes <gabrielgimenes157@gmail.com>
2024-03-06 08:47:33 -0300

    feat: Data Base - Creating queries with knex

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import fastify from "fastify";
+import crypto, { randomUUID } from "node:crypto"
 import { knex } from "./database";
 
 const app = fastify();
 
 
 app.get('/hello', async() => {
-    const tables = await knex('sqlite_schema').select('*')
+    const transactions = await knex('transactions').where('amount', 1000).select('*')
 
-    return tables
+    return transactions
 })
 
 app.listen({
     port:3333,
 }).then(() => {
     console.log('HTTP Server Running')
-})
+}) 
