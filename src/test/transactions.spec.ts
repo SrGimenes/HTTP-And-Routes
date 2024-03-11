1cb5797ccfd9739f5cf047b41d76a7cd669d0925
GabriellGimenes <gabrielgimenes157@gmail.com>
2024-03-11 08:34:53 -0300

    feat: Automated tests - Testing transactions listing

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,16 +1,17 @@
-import { it, beforeAll, afterAll, describe } from "vitest";
+import { it, beforeAll, afterAll, describe, expect } from "vitest";
 import request from "supertest";
 import { app } from "../app";
+import exp from "constants";
 
-describe('Transactions routes', ()=> {
+describe("Transactions routes", () => {
   beforeAll(async () => {
     await app.ready();
   });
-  
+
   afterAll(async () => {
     await app.close();
   });
-  
+
   it("should be able to create a new transaction", async () => {
     await request(app.server)
       .post("/transactions")
@@ -21,5 +22,29 @@ describe('Transactions routes', ()=> {
       })
       .expect(201);
   });
-})
 
+  it("should be able to list all transactions", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+      expect(listTransactionsResponse.body.transactions).toEqual([
+        expect.objectContaining({
+          title: "New transaction",
+          amount: 5000,
+        })
+      ])
+  });
+
+});
