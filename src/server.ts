e2bcdd4bfab871fb122c630a4dc3671f0aa3e6d6
GabriellGimenes <gabrielgimenes157@gmail.com>
2024-03-12 13:24:11 -0300

    fix: production config server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { app } from "./app";
 import { env } from "./env";
 
-
 app
   .listen({
+    host: "RENDER" in process.env ? "0.0.0.0" : "localhost",
     port: env.PORT,
   })
   .then(() => {
